c0b4931ba39fad5c31e49f56337b90d8113aa10c
Jake Parker <jake.l.parker@gmail.com>
2018-05-01 17:17:54 -0400

    fixed pickle file loading

--- train_test_split_search.py
+++ train_test_split_search.py
@@ -15,7 +15,9 @@ fracs = range(0.1, 0.8, 0.1)
 for dataset in datasets:
     for model in models:
         # load hyper_parameters from pickle file creatd by `hyper_param_search.py`
-        hyper_parameters = pickle.load(dataset + '_' + model + '_hyper_parameters.pkl')
+        # - molenet params: dataset + model + '.pkl'
+        # - hyper param search:  dataset + '_' + model + '_hyper_parameters.pkl'
+        hyper_parameters = pickle.load(os.path.join('.', 'pickle', dataset + model + '.pkl'))
         for frac in fracs:
             run_train_test_benchmark(datasets=[dataset],
                                      split='random', 
