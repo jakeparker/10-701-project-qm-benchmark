6306684fba68c1a73343c9be993705a2ddc83dce
EC2 Default User <ec2-user@ip-172-31-82-134.ec2.internal>
2018-05-02 02:19:40 +0000

    finished qm7 training splits

--- train_test_split_search.py
+++ train_test_split_search.py
@@ -3,12 +3,13 @@ import pickle
 import numpy as np
 import tensorflow as tf
 import deepchem as dc
+from deepchem.molnet.preset_hyper_parameters import hps
 from train_test_benchmark import run_train_test_benchmark
 
 np.random.seed(123)
 tf.set_random_seed(123)
 
-datasets = ['qm7', 'qm7b', 'qm8', 'qm9']
+datasets = ['qm7b', 'qm8', 'qm9'] # 'qm7'
 models = ['rf_regression', 'krr', 'graphconvreg', 'weave_regression']
 fracs = [float(x+1)/10 for x in range(8)]
 
@@ -18,8 +19,11 @@ for dataset in datasets:
     # - molenet params: dataset + model + '.pkl'
     # - hyper param search:  dataset + '_' + model + '_hyper_parameters.pkl'
     
-    with open(os.path.join('.', 'pickle', dataset + model + '.pkl'), 'rb') as f:
-        hyper_parameters = pickle.load(f)
+    try:    
+        with open(os.path.join('.', 'pickle', dataset + model + '.pkl'), 'rb') as f:
+            hyper_parameters = pickle.load(f)
+    except:
+        hyper_parameters = hps[model]
     for frac in fracs:
       run_train_test_benchmark(datasets=[dataset],
                                split='random', 
